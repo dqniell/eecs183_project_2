064ea100102eb8eb863f770a61b31ec98e65cee0
Daniel Lee <joonleee@umich.edu>
2024-09-18 21:37:50 -0400

    Initial Commit

---
+++ eecs183_project2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  eecs183_project2
+//
+//  Created by Daniel Lee on 9/18/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
